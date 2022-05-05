8db5e036b4f598c19689111e7f286c8f19e3077f
masoumehsalami <salami.masy@gmail.com>
2022-05-05 19:53:16 +0430

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -9,13 +9,17 @@ export default  function App() {
       <h1> 
           helloooooo weather
       </h1>
+      <div className='container'>
+
       <footer>
       This Project was coded by Masoumeh Salami and 
+      <br/>
       is located in 
       <a href='https://github.com/masoumehsalami/react-weather-app'>
        Git Add
       </a>
       </footer>
+      </div>
     </div>
   );
 }
