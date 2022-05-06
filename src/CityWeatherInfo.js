a9bf80211544e0c110a485a9b4e018c55b2070ff
masoumehsalami <salami.masy@gmail.com>
2022-05-06 14:19:21 +0430

    add search function

---
+++ src/CityWeatherInfo.js
@@ -0,0 +1,45 @@
+import React from "react";
+import LocalTime from "./LocalTime";
+
+export default function CityWeatherInfo(props){
+return(
+  <div className="weatherInfo"> 
+   <h1>
+     {props.data.cityname}
+   </h1>
+
+    <ul>
+    <li>
+        Local time:<LocalTime date= {props.data.date}/>
+    </li>
+    <br/>
+    <li className="text-capitalize">
+       {props.data.description}
+    </li>
+    </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img src="http://openweathermap.org/img/wn/" alt="MostlyClouldy" className="float-left"/> 
+              <div className="float-left">
+                 <span className="temprature">{Math.round(props.data.temp)}</span>  
+                <span className="unit">°c</span>
+               </div>
+            </div>
+         </div>
+        <div  className="col-6">
+        <ul>
+            <li>
+                precipitaion : 15%0
+            </li>
+            <li>
+               humidity:{props.data.humidity} %
+            </li>
+            <li>
+                wind:{props.data.wind} km/h
+            </li>
+        </ul>
+        </div>
+     </div> 
+    </div>);
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,9 @@
 import React,{useState} from "react";
 import "./Weather.css";
 import axios from "axios";
-import LocalTime from "./LocalTime";
+import CityWeatherInfo from "./CityWeatherInfo";
 export default function Weather(props){
-  
+    const[city,setCity]=useState(props.defaultCity)  
     const [weatherTemp,setWeatherTemp]   = useState({ready:false})
     function handleResponse(response){
 
@@ -19,71 +19,43 @@ export default function Weather(props){
         });
        
     }
+    
+    function search(){
+        const apikey="3974d69436c99c72f5f2388d04551db4";
+        let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+    } 
+    
+    function handler(event){
+       event.preventDefault();
+       search(city);
+    }
+    function handlerCity(event){
+        setCity(event.target.value)
+    }
     if (weatherTemp.ready){
-
-
     return(
     <div className="Weather">
         Amazing Weather search Engine
-        <form>
+        <form onSubmit={handler}>
            <div className="row">
                <div className="col-9">
                    <input type="search"
                    placeholder="Enter a City..."
                    className="form-control"
-                   autoFocus="on"/>
+                   autoFocus="on"
+                   onChange={handlerCity}/>
                </div>
                <div className="col-3">
                    <input type="submit" value="search" className="btn btn-primary w-100"/>
                </div>
            </div>
         </form>
- <h1>
-     {weatherTemp.cityname}
- </h1>
-        <ul>
-            <li>
-                Local time:<LocalTime date= {weatherTemp.date}/>
-            </li>
-            <br/>
-            <li className="text-capitalize">
-               {weatherTemp.description}
-            </li>
-        </ul>
-        <div className="row mt-3">
-            <div className="col-6">
-                <div className="clearfix">
-                <img src="http://openweathermap.org/img/wn/" alt="MostlyClouldy" className="float-left"/> 
-                  <div className="float-left">
-                    <span className="temprature">{Math.round(weatherTemp.temp)}</span>  
-                    <span className="unit">°c</span>
-                </div>
-                </div>
-            </div>
-            <div  className="col-6">
-                <ul>
-                    <li>
-                        precipitaion : 15%0
-                    </li>
-                    <li>
-                       humidity:{weatherTemp.humidity} %
-                    </li>
-                    <li>
-                        wind:{weatherTemp.wind} km/h
-                    </li>
-                </ul>
-            </div>
-        </div>
-        
-    
-    </div>
-    )
+        <CityWeatherInfo data={weatherTemp}/>    
+     </div>
+    );
 } else{
-    
-    const apikey="3974d69436c99c72f5f2388d04551db4";
-    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apikey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-    
-    return "loading..."
+    search();
+    return "loading...";
 }
-}
+ }
